6f84f86f18c857d7d6b31f69452204eb1acc59bf
khactutnkt <khactut@gmail.com>
2021-11-15 20:30:30 +0700

    15/11/2021

--- src/ss8_clean_code_and_refactoring/bai_tap/TennisGame.java
+++ src/ss8_clean_code_and_refactoring/bai_tap/TennisGame.java
@@ -2,60 +2,75 @@ package ss8_clean_code_and_refactoring.bai_tap;
 
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(String player1Name, String player2Name, int playerScore1, int playerScore2) {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        int tempScore = 0;
+        boolean hoa = playerScore1 == playerScore2;
+        boolean advantage = playerScore1 >= 4 || playerScore2 >= 4;
+        if (hoa) {
+            score = tiSoKhiHoa(playerScore1);
+        } else if (advantage) {
+            score = tiSoCuoiCung(playerScore1, playerScore2);
+        } else {
+            score = tiSoHienTai(playerScore1, playerScore2);
+        }
+        return score;
+    }
+
+    public static String tiSoKhiHoa(int playerScore1) {
+        String score;
+        switch (playerScore1) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
+    }
+
+    public static String tiSoCuoiCung(int playerScore1, int playerScore2) {
+        String score;
+        int minusResult = playerScore1 - playerScore2;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
+
+    public static String tiSoHienTai(int playerScore1, int playerScore2) {
+        int tempScore;
+        String score = "";
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = playerScore1;
+            else {
+                score += "-";
+                tempScore = playerScore2;
+            }
+            switch (tempScore) {
                 case 0:
-                    score = "Love-All";
+                    score += "Love";
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score += "Fifteen";
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score += "Thirty";
                     break;
                 case 3:
-                    score = "Forty-All";
+                    score += "Forty";
                     break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
             }
         }
         return score;
--- src/ss8_clean_code_and_refactoring/bai_tap/TennisGameTest.java
+++ src/ss8_clean_code_and_refactoring/bai_tap/TennisGameTest.java
@@ -67,14 +67,14 @@ public class TennisGameTest {
     @Test
     public void checkAllScores() {
         int highestScore = Math.max(this.player1Score, this.player2Score);
-        int m_score1 = 0;
-        int m_score2 = 0;
+        int playerScore1 = 0;
+        int playerScore2 = 0;
         for (int i = 0; i < highestScore; i++) {
             if (i < this.player1Score)
-                m_score1 += 1;
+                playerScore1 += 1;
             if (i < this.player2Score)
-                m_score2 += 1;
+                playerScore2 += 1;
         }
-        assertEquals(this.expectedScore, TennisGame.getScore("John", "Bill", m_score1, m_score2));
+        assertEquals(this.expectedScore, TennisGame.getScore("John", "Bill", playerScore1, playerScore2));
     }
 }
