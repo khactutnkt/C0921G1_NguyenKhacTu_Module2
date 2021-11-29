59f1ff90dc79e1278623530232152c8efd2e8594
khactutnkt <khactut@gmail.com>
2021-11-29 17:34:43 +0700

    29/11/2021

---
+++ src/Module_2_CaseStudy/views/Test.java
@@ -0,0 +1,7 @@
+import controllers.FuramaController;
+public class Test {
+    public static void main(String[] args) {
+        FuramaController f = new FuramaController();
+        f.displayMainMenu();
+    }
+}
